d92a0903a28e97210ed47025c56bd755afe2d96b
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-18 13:51:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// **Consegna**
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+/** SVOLGIMENTO
+Al clic su button play
+    Nacondere il titolo h2
+    Mostrare la griglia, quindi prima devo generarla
+        Creare dinamicamente 100 square
+            creare l'elemento
+            aggiungere la classe
+            inserire il numero che è nello span
+            al click sullo square
+                aggiungere click listener --> al click cambia colore e stampa numero in console
+            appendo lo square alla grid
+    Dopo che la griglia è stata generata devo mostrarla
+*/
